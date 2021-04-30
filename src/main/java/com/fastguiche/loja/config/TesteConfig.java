0f0c157ae9076c739922f82837092c63a12650df
andreltcarvalho <andreltcarvalho@hotmail.com>
2021-04-30 14:50:15 -0300

    tabela products_categories

--- src/main/java/com/fastguiche/loja/config/TesteConfig.java
+++ src/main/java/com/fastguiche/loja/config/TesteConfig.java
@@ -46,9 +46,20 @@ public class TesteConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		
+		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
