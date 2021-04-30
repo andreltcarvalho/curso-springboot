e8ffed9ee461e6363a22ec6fbf7f22342c24255a
andreltcarvalho <andreltcarvalho@hotmail.com>
2021-04-29 23:47:11 -0300

    H2 database test

---
+++ src/main/java/com/fastguiche/loja/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.fastguiche.loja.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fastguiche.loja.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "maria", "maria@gmail.com", "99999999", "12345");
+		
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
